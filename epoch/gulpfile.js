119669f6bc4166791d7c856773bd8cfd47f93126
d10cn2btt <d10cn2btt@gmail.com>
2016-07-03 04:07:06 +0700

    setup blank project with swig

--- epoch/gulpfile.js
+++ epoch/gulpfile.js
@@ -1,37 +1,38 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var swig = require('gulp-swig');
 
-/*
-gulp.task('sass', function(){
-  return gulp.src('source-files')
-    .pipe(sass()) // Using gulp-sass
-    .pipe(gulp.dest('destination'))
+gulp.task('swig', function() {
+    return gulp.src('app/swig/**/*.html')
+        .pipe(swig({defaults: { cache: false }}))
+        .pipe(gulp.dest('app/html'))
 });
-*/
 
-gulp.task('sass', function() {
-  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
-    .pipe(sass())// Converts Sass to CSS with gulp-sass
-    .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+gulp.task('sass', function () {
+    return gulp.src('app/assets/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+        .pipe(sass())// Converts Sass to CSS with gulp-sass
+        .pipe(gulp.dest('app/assets/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        }
+    })
 });
 
 //gulp.watch('files-to-watch', ['tasks', 'to', 'run']); 
 
-gulp.task('watch', ['browserSync', 'sass'], function(){
-  gulp.watch('app/scss/**/*.scss', ['sass']); 
-  // Reloads the browser whenever HTML or JS files change
-  gulp.watch('app/*.html', browserSync.reload); 
-  gulp.watch('app/js/**/*.js', browserSync.reload); 
-})
-
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: 'app'
-    },
-  })
-})
+gulp.task('watch', ['browserSync', 'sass', 'swig'], function () {
+    gulp.watch('app/assets/scss/**/*.scss', ['sass']);
+    gulp.watch('app/swig/*.html', ['swig']);
+    // gulp.watch('app/twig/*.twig', ['twig']);
+    // Reloads the browser whenever HTML or JS files change
+    gulp.watch('app/**/*.html', browserSync.reload);
+    gulp.watch('app/assets/js/**/*.js', browserSync.reload);
+});
